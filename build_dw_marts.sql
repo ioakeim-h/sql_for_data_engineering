99ab9b0e12fa5d12e38d0c3bca3349d51b77bd9c
ioakeim-h <ioakeim.h@gmail.com>
2026-07-02 15:12:47 +0300

    build and validate flat mart

--- build_dw_marts.sql
+++ build_dw_marts.sql
@@ -1,7 +1,11 @@
--- Step 0 - Run in CLI: duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
+-- Step 0: DB setup
+-- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
 
 -- Step 1: DW - Create star schema tables
 .read 01_create_tables_dw.sql
 
 -- Step 2: DW - Load data from CSV files into tables
 .read 02_load_schema_dw.sql
+
+-- Step 3: Mart - Create flat mart
+.read 03_create_flat_mart.sql
